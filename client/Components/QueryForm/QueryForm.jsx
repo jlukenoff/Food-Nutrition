c723eb31efb011790b0694ff85752d5dd88ccc4c
John Lukenoff <jlukenoff@gmail.com>
2018-10-01 09:02:46 -0700

    Change input field type to number

--- client/Components/QueryForm/QueryForm.jsx
+++ client/Components/QueryForm/QueryForm.jsx
@@ -12,30 +12,30 @@ const QueryForm = ({
     <div className={styles.inputContainer}>
       <div className={styles.inputFieldContainer}>
         <p>Protein</p>
-        <input type="text" placeholder="min" className={styles.inputField} onKeyUp={e => handleInput('proteinMin', +e.target.value)} />
+        <input type="number" min="0" placeholder="min" className={styles.inputField} onKeyUp={e => handleInput('proteinMin', +e.target.value)} />
         <span> gm - </span>
-        <input type="text" placeholder="max" className={styles.inputField} onKeyUp={e => handleInput('proteinMax', +e.target.value)} />
+        <input type="number" min="0" placeholder="max" className={styles.inputField} onKeyUp={e => handleInput('proteinMax', +e.target.value)} />
         {' gm'}
       </div>
       <div className={styles.inputFieldContainer}>
         <p>Fat</p>
-        <input type="text" placeholder="min" className={styles.inputField} onKeyUp={e => handleInput('fatMin', +e.target.value)} />
+        <input type="number" min="0" placeholder="min" className={styles.inputField} onKeyUp={e => handleInput('fatMin', +e.target.value)} />
         <span> gm - </span>
-        <input type="text" placeholder="max" className={styles.inputField} onKeyUp={e => handleInput('fatMax', +e.target.value)} />
+        <input type="number" min="0" placeholder="max" className={styles.inputField} onKeyUp={e => handleInput('fatMax', +e.target.value)} />
         {' gm'}
       </div>
       <div className={styles.inputFieldContainer}>
         <p>Carbohydrate, by difference</p>
-        <input type="text" placeholder="min" className={styles.inputField} onKeyUp={e => handleInput('carbMin', +e.target.value)} />
+        <input type="number" min="0" placeholder="min" className={styles.inputField} onKeyUp={e => handleInput('carbMin', +e.target.value)} />
         <span> gm - </span>
-        <input type="text" placeholder="max" className={styles.inputField} onKeyUp={e => handleInput('carbMax', +e.target.value)} />
+        <input type="number" min="0" placeholder="max" className={styles.inputField} onKeyUp={e => handleInput('carbMax', +e.target.value)} />
         {' gm'}
       </div>
       <div className={styles.inputFieldContainer}>
         <p>Sugar</p>
-        <input type="text" placeholder="min" className={styles.inputField} onKeyUp={e => handleInput('sugarMin', +e.target.value)} />
+        <input type="number" min="0" placeholder="min" className={styles.inputField} onKeyUp={e => handleInput('sugarMin', +e.target.value)} />
         <span> gm - </span>
-        <input type="text" placeholder="max" className={styles.inputField} onKeyUp={e => handleInput('sugarMax', +e.target.value)} />
+        <input type="number" min="0" placeholder="max" className={styles.inputField} onKeyUp={e => handleInput('sugarMax', +e.target.value)} />
         {' gm'}
       </div>
     </div>
